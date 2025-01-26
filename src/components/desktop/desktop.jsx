c33126f20861448a8dc28f1f2e808135278b690c
Jonas <jonasneu812@gmail.com>
2025-01-26 11:54:55 +0100

    fixed: desktop view for dev

--- src/components/desktop/desktop.jsx
+++ src/components/desktop/desktop.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
-import styles from './desktop.module.css';
-import { Taskbar } from '../desktopUIComponents/taskbar/taskbar';
-import { Window } from '../desktopUIComponents/window/window';
-import { Screensaver } from '../desktopUIComponents/screensaver/screensaver';
+import React, { useState } from "react";
+import styles from "./desktop.module.css";
+import { Taskbar } from "../desktopUIComponents/taskbar/taskbar";
+import { Window } from "../desktopUIComponents/window/window";
+import { Screensaver } from "../desktopUIComponents/screensaver/screensaver";
 
 export const Desktop = ({ isFullscreen }) => {
   const [showIframe, setShowIframe] = useState(false);
 
   return (
     <>
-      { !isFullscreen && (
-        <img 
-          src="monitor.png"
-          id={styles.monitor}
-        />
-      )}
+      {/* {!isFullscreen && <img src="monitor.png" id={styles.monitor} />} */}
 
-      { !isFullscreen && (
-        <Screensaver />
-      )}
+      {!isFullscreen && <Screensaver />}
 
-      <div id={ isFullscreen ? styles.fullscreen : styles.standart }>
-        <Window x='10' y='150' z='50' width='800' height='300'/>
+      <div id={isFullscreen ? styles.fullscreen : styles.standart}>
+        <Window x="10" y="150" z="50" width="800" height="300" />
         <Taskbar />
       </div>
     </>
